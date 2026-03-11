45ae3a013f8b9a40003f590af899ad7be8779780
DarkWins <DarkWins@NINGUNO>
2026-03-11 14:24:58 -0500

    ultimo

--- ProyectoServices/Interfaces/IMeseroService.cs
+++ ProyectoServices/Interfaces/IMeseroService.cs
@@ -4,12 +4,12 @@ using ProyectoModelo;
 
 namespace ProyectoServices
 {
-    public interface IMeseroService
-    {
-        //Task<IEnumerable<Mesero>> GetAllAsync();
-        //Task<Mesero?> GetByIdAsync(int id);
-        //Task<Mesero> CreateAsync(Mesero mesero);
-        //Task<Mesero> UpdateAsync(Mesero mesero);
-        //Task<bool> DeleteAsync(int id);
-    }
+    //public interface IMeseroService
+    //{
+    //    //Task<IEnumerable<Mesero>> GetAllAsync();
+    //    //Task<Mesero?> GetByIdAsync(int id);
+    //    //Task<Mesero> CreateAsync(Mesero mesero);
+    //    //Task<Mesero> UpdateAsync(Mesero mesero);
+    //    //Task<bool> DeleteAsync(int id);
+    //}
 }
